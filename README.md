1391e6dde560930e1d44d91161dc6a59bae26036
Alex C-G <alexcg@outlook.com>
2021-09-20 17:12:11 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# FaceDetector
+
+Detects if a human face is in an image Document.
+
+**Note**: It *detects* human faces. It doesn't *recognize* individuals.
+
+In the words of the late, great [Terry Pratchett](http://www.chrisjoneswriting.com/terry-pratchett-quotes/technology-terry-pratchett-quote):
+
+> Oh, well, if you prefer, I can recognize handwriting,’ said the imp proudly.  ‘I’m quite advanced.’
+> Vimes pulled out his notebook and held it up. ‘Like this?’ he said.
+> The imp squinted for a moment. ‘Yep,’ it said. ‘That’s handwriting, sure enough. Curly bits, spiky bits, all joined together. Yep. Handwriting. I’d recognize it anywhere.'
+
+## Usage
+
+#### via Docker image (recommended)
+
+```python
+from jina import Flow
+	
+f = Flow().add(uses='jinahub+docker://FaceDetector')
+```
+
+#### via source code
+
+```python
+from jina import Flow
+	
+f = Flow().add(uses='jinahub://FaceDetector')
+```
+
+- To override `__init__` args & kwargs, use `.add(..., uses_with: {'key': 'value'})`
+- To override class metas, use `.add(..., uses_metas: {'key': 'value})`
---
+++ executor.py
@@ -0,0 +1,42 @@
+from jina import Executor, DocumentArray, requests
+import face_recognition
+from PIL import Image
+
+def check_human(image_uri):
+    """check_human.
+
+    Detects if there's one or more humans in the image. 
+    :param image_uri: Path to image file
+    """
+    image = face_recognition.load_image_file(image_uri)
+    face_locations = face_recognition.face_locations(image)
+    if face_locations:
+        return True
+    else:
+        return False
+
+class FaceDetector(Executor):
+    @requests(on="/search")
+    def check_human(self, docs: DocumentArray, **kwargs):
+        """check_human.
+        Checks for a human in an image Document, and writes True or False to doc.tags["is_human"]
+        :param docs:
+        :type docs: DocumentArray
+        :param kwargs:
+        """
+        for doc in docs:
+            # First ensure we have a blob
+            if hasattr(doc, "uri"):
+                doc.convert_uri_to_blob()
+            elif hasattr(doc, "buffer"):
+                doc.convert_buffer_to_blob()
+
+            assert hasattr(doc, "blob")
+
+            # Dump blob to image file
+            image = Image.fromarray(doc.blog)
+            image.save("doc_content.jpg")
+
+            doc.tags["is_human"] = check_human("doc_content.jpg")
+
+
