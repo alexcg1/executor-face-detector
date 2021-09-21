0ea615eb1214b929edbbdde8241acd1ae1a5eb0b
Alex C-G <alexcg@outlook.com>
2021-09-21 12:57:54 +0200

    feat: tests, skip non humans option

--- executor.py
+++ executor.py
@@ -1,23 +1,29 @@
 from jina import Executor, DocumentArray, requests
+import numpy as np
 import face_recognition
 from PIL import Image
 import os
 
+
 def check_human(image_uri):
     """check_human.
 
-    Detects if there's one or more humans in the image. 
+    Detects if there's one or more humans in the image.
     :param image_uri: Path to image file
     """
     image = face_recognition.load_image_file(image_uri)
     face_locations = face_recognition.face_locations(image)
     if face_locations:
         return True
-    else:
-        return False
+    return False
+
 
 class FaceDetector(Executor):
-    @requests(on="/search")
+    def __init__(self, skip_non_faces, **kwargs):
+        super().__init__(**kwargs)
+        self.skip_non_faces = skip_non_faces
+
+    @requests
     def check_human(self, docs: DocumentArray, **kwargs):
         """check_human.
         Checks for a human face in an image Document, and writes True or False to doc.tags["is_human"]
@@ -25,18 +31,20 @@ class FaceDetector(Executor):
         :type docs: DocumentArray
         :param kwargs:
         """
-        for doc in docs:
-            # First ensure we have a blob
-            if hasattr(doc, "uri"):
-                doc.convert_uri_to_blob()
-            elif hasattr(doc, "buffer"):
-                doc.convert_buffer_to_blob()
-
+        docs_to_remove = []
+        for idx, doc in enumerate(docs):
             assert hasattr(doc, "blob")
 
             # Dump blob to image file
-            image = Image.fromarray(doc.blog)
+            image = Image.fromarray((doc.blob * 255).astype(np.uint8))
             image.save("doc_content.jpg")
 
             doc.tags["is_human"] = check_human("doc_content.jpg")
             os.remove("doc_content.jpg")
+
+            if self.skip_non_faces == True and doc.tags["is_human"] == False:
+                docs_to_remove.append(idx)
+
+        docs_to_remove.reverse()
+        for idx in docs_to_remove:
+            del docs[idx]
---
+++ testing.py
@@ -0,0 +1,22 @@
+from jina import Document, DocumentArray, Flow
+from jina.types.document.generators import from_files
+from executor import FaceDetector
+
+docs = DocumentArray(from_files("test_data/**/*.jpg"))
+
+for doc in docs:
+    doc.tags["uri"] = doc.uri
+    doc.convert_image_uri_to_blob()
+
+flow = (
+    Flow()
+    .add(uses="jinahub+docker://ImageNormalizer")
+    .add(uses=FaceDetector, uses_with={"skip_non_faces": False})
+)
+
+with flow:
+    output = flow.index(inputs=docs, return_results=True)
+
+for doc in output[0].docs:
+    manual_label = doc.tags["uri"].split("/")[1]
+    assert manual_label == str(doc.tags["is_human"])
