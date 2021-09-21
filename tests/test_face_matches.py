49fd99cf36b2e6d965078999bae2960798afc4df
Alex C-G <alexcg@outlook.com>
2021-09-21 13:54:26 +0200

    refactor: make code clearer

--- tests/test_face_matches.py
+++ tests/test_face_matches.py
@@ -1,6 +1,5 @@
 from jina import Document, DocumentArray, Flow
 from jina.types.document.generators import from_files
-# from ..executor import FaceDetector
 from .. import FaceDetector
 
 docs = DocumentArray(from_files("test_data/**/*.jpg"))
@@ -18,7 +17,9 @@ flow = (
 with flow:
     output = flow.index(inputs=docs, return_results=True)
 
+# I want it to test each doc in the list, but don't know how to do that in pytest
 def test_matches():
     for doc in output[0].docs:
-        manual_label = doc.tags["uri"].split("/")[1]
-        assert manual_label == str(doc.tags["is_human"])
+        manual_label = doc.tags["uri"].split("/")[1] # Label added by hand (i.e. name of subfolder)
+        machine_label = str(doc.tags["is_human"]) # Label added by executor. Cast to string so same as manual labels
+        assert manual_label == machine_label
